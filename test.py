d249be5da92bf88f016eaad70098aeaebb36a815
viols-code <viola.renne@gmail.com>
2022-02-21 00:24:34 +0100

    Miglioramento dello stile del codice

--- test.py
+++ test.py
@@ -4,31 +4,7 @@ import math
 import pandas as pd
 from pandas import DataFrame
 
-# Non capisco a cosa serva
-# pd.set_option('display.max_columns', None)
-
-#leggo il file pdb
-ppdb:PandasPdb = PandasPdb().read_pdb('./protein.pdb')
-ppdb.df['ATOM'] = ppdb.df['ATOM'][ppdb.df['ATOM']['element_symbol'] != 'H']
-# stampo per controllo
-# print(ppdb.df)
-
-# M è un'istanza della classe ndarray
-matrix = ppdb.df['ATOM'][['x_coord', 'y_coord', 'z_coord', 'element_symbol']].to_numpy()
-
-# questo comando messo qua non ha senso
-# ppdb.df['ATOM']
-# print(ppdb)
-
-# rendo tutte le coordinate positive
-for i in range(len(matrix)):
-    matrix[i][0] += 50
-    matrix[i][1] += 50
-    matrix[i][2] += 50
-
-# creo una griglia di soli zeri
-grid = np.zeros((200, 200, 200))
-
+#inizializzo i raggi dei vari atomi
 raggio = {
     'N' : 1.6,
     'O' : 1.5,
@@ -37,42 +13,90 @@ raggio = {
     'S' : 1.8,
 }
 
+def letturaFile():
+    # chiedo all'utente di inserire il path del file pdb
+    print('Inserisci il path del file pdb')
+    path = input()
+    # leggo il file pdb
+    ppdb:PandasPdb = PandasPdb().read_pdb(path)
+    return ppdb
 
-# metto gli uni
-#raggio[matrix[i][3]]
-for i in range(len(matrix)):
-    intx = int(matrix[i][0])
-    inty = int(matrix[i][1])
-    intz = int(matrix[i][2])
-    for j in range(intx - 2, intx + 3):
-        for k in range(inty - 2, inty + 3):
-            for t in range(intz - 2, intz + 3):
-                if math.sqrt((matrix[i][0] - j) ** 2 + (matrix[i][1] - k) ** 2 + (matrix[i][2] - t) ** 2) < raggio[matrix[i][3]]:
-                    grid[j][k][t] = 1
+def eliminazioneIdrogeni(ppdb):
+    #elimino gli idrogeni
+    ppdb.df['ATOM'] = ppdb.df['ATOM'][ppdb.df['ATOM']['element_symbol'] != 'H']
+    return ppdb
+
+def inizializzazioneMatrice():
+    # matrix è un'istanza della classe ndarray
+    matrix = ppdb.df['ATOM'][['x_coord', 'y_coord', 'z_coord', 'element_symbol']].to_numpy()
+
+    # per avere tutte le coordinate positive
+    for i in range(len(matrix)):
+        matrix[i][0] += 50
+        matrix[i][1] += 50
+        matrix[i][2] += 50
+
+    return matrix
+
+def inizializzazioneGriglia():
+    # creo una griglia di soli zeri
+    grid = np.zeros((200, 200, 200))
+
+    # inserisco gli uni in posizione degli atomi
+    # raggio[matrix[i][3]]
+    for i in range(len(matrix)):
+        intx = int(matrix[i][0])
+        inty = int(matrix[i][1])
+        intz = int(matrix[i][2])
+        for j in range(intx - 2, intx + 3):
+            for k in range(inty - 2, inty + 3):
+                for t in range(intz - 2, intz + 3):
+                    if math.sqrt((matrix[i][0] - j) ** 2 + (matrix[i][1] - k) ** 2 + (matrix[i][2] - t) ** 2) < raggio[
+                        matrix[i][3]]:
+                        grid[j][k][t] = 1
+
+    return grid
+
+def fromDataFrametoPdb(list):
+    pp: DataFrame = pd.DataFrame(list,
+                                 columns=['record_name', 'atom_number', 'blank_1', 'atom_name', 'alt_loc',
+                                          'residue_name', 'blank_2',
+                                          'chain_id', 'residue_number', 'insertion', 'blank_3', 'x_coord', 'y_coord',
+                                          'z_coord',
+                                          'occupancy', 'b_factor', 'blank_4', 'segment_id', 'element_symbol', 'charge',
+                                          'line_idx'])
+
+    ppdb = PandasPdb()
+    ppdb.df['ATOM'] = pp
+
+    return ppdb
+
+def creazioneListaAtomi():
+    list = []
+    count = 1
+    for i in range(200):
+        for j in range(200):
+            for k in range(200):
+                if (grid[i][j][k] == 1):
+                    list.append(
+                        ('ATOM', count, '', 'H', '', 'ILE', '', 'A', count, '', '', i - 50, j - 50, k - 50, 1.0, 0,
+                         '', '', 'H', 0, count))
+                    count += 1
+
+    return list
 
 
-u = []
-count = 1
-for i in range(200):
-    for j in range(200):
-        for k in range(200):
-            if (grid[i][j][k] == 1):
-                u.append(('ATOM', count, '', 'H', '', 'ILE', '', 'A', count, '', '', i-50, j-50, k-50, 1.0, 0,
-                          '', '', 'H', 0, count))
-                count += 1
 
-#('ATOM', count, '', 'H', '', '', 'A', '', count, '', '', i-50, j-50, k-50, 1.0, 0, '', '', 'H', '', '')
+ppdb = letturaFile()
+ppdb = eliminazioneIdrogeni(ppdb)
 
+matrix = inizializzazioneMatrice()
 
-# record_name    atom_number    blank_1    atom_name    alt_loc    residue_name    blank_2    chain_id    residue_number    insertion    blank_3    x_coord    y_coord    z_coord    occupancy    b_factor    blank_4    segment_id    element_symbol    charge    line_idx
-pp:DataFrame = pd.DataFrame(u,
-                    columns=['record_name', 'atom_number', 'blank_1', 'atom_name', 'alt_loc', 'residue_name', 'blank_2',
-                             'chain_id', 'residue_number', 'insertion', 'blank_3', 'x_coord', 'y_coord', 'z_coord',
-                             'occupancy', 'b_factor', 'blank_4', 'segment_id', 'element_symbol', 'charge', 'line_idx'])
+grid = inizializzazioneGriglia()
 
+list = creazioneListaAtomi()
 
-pppdb = PandasPdb()
-pppdb.df['ATOM'] = pp
+pppdb = fromDataFrametoPdb(list)
 
 pppdb.to_pdb(path='./3eiy_stripped4.pdb',
             records=['ATOM'],
