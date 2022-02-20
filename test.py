2a5d0022fd790ca7874f6cbae5fe643e999ed358
viols-code <viola.renne@gmail.com>
2022-02-21 00:45:50 +0100

    Aggiunta di funzioni al fine di migliorare la leggibilità del codice

--- test.py
+++ test.py
@@ -4,29 +4,32 @@ import math
 import pandas as pd
 from pandas import DataFrame
 
-#inizializzo i raggi dei vari atomi
+# inizializzo i raggi dei vari atomi
 raggio = {
-    'N' : 1.6,
-    'O' : 1.5,
-    'C' : 1.7,
-    'F' : 1.8,
-    'S' : 1.8,
+    'N': 1.6,
+    'O': 1.5,
+    'C': 1.7,
+    'F': 1.8,
+    'S': 1.8,
 }
 
-def letturaFile():
+
+def lettura_file():
     # chiedo all'utente di inserire il path del file pdb
     print('Inserisci il path del file pdb')
     path = input()
     # leggo il file pdb
-    ppdb:PandasPdb = PandasPdb().read_pdb(path)
+    ppdb: PandasPdb = PandasPdb().read_pdb(path)
     return ppdb
 
-def eliminazioneIdrogeni(ppdb):
-    #elimino gli idrogeni
+
+def eliminazione_idrogeni(ppdb):
+    # elimino gli idrogeni
     ppdb.df['ATOM'] = ppdb.df['ATOM'][ppdb.df['ATOM']['element_symbol'] != 'H']
     return ppdb
 
-def inizializzazioneMatrice():
+
+def inizializzazione_matrice(ppdb):
     # matrix è un'istanza della classe ndarray
     matrix = ppdb.df['ATOM'][['x_coord', 'y_coord', 'z_coord', 'element_symbol']].to_numpy()
 
@@ -38,7 +41,12 @@ def inizializzazioneMatrice():
 
     return matrix
 
-def inizializzazioneGriglia():
+
+def distanza3D(x1, x2, y1, y2, z1, z2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+
+def inizializzazione_griglia(matrix):
     # creo una griglia di soli zeri
     grid = np.zeros((200, 200, 200))
 
@@ -51,14 +59,29 @@ def inizializzazioneGriglia():
         for j in range(intx - 2, intx + 3):
             for k in range(inty - 2, inty + 3):
                 for t in range(intz - 2, intz + 3):
-                    if math.sqrt((matrix[i][0] - j) ** 2 + (matrix[i][1] - k) ** 2 + (matrix[i][2] - t) ** 2) < raggio[
-                        matrix[i][3]]:
+                    if distanza3D(matrix[i][0], j, matrix[i][1], k, matrix[i][2], t) < raggio[matrix[i][3]]:
                         grid[j][k][t] = 1
 
     return grid
 
-def fromDataFrametoPdb(list):
-    pp: DataFrame = pd.DataFrame(list,
+
+def creazione_lista_atomi(grid):
+    list2 = []
+    count = 1
+    for i in range(200):
+        for j in range(200):
+            for k in range(200):
+                if grid[i][j][k] == 1:
+                    list2.append(
+                        ('ATOM', count, '', 'H', '', 'ILE', '', 'A', count, '', '', i - 50, j - 50, k - 50, 1.0, 0,
+                         '', '', 'H', 0, count))
+                    count += 1
+
+    return list2
+
+
+def fromdataframe_topdb(list1):
+    pp: DataFrame = pd.DataFrame(list1,
                                  columns=['record_name', 'atom_number', 'blank_1', 'atom_name', 'alt_loc',
                                           'residue_name', 'blank_2',
                                           'chain_id', 'residue_number', 'insertion', 'blank_3', 'x_coord', 'y_coord',
@@ -71,34 +94,19 @@ def fromDataFrametoPdb(list):
 
     return ppdb
 
-def creazioneListaAtomi():
-    list = []
-    count = 1
-    for i in range(200):
-        for j in range(200):
-            for k in range(200):
-                if (grid[i][j][k] == 1):
-                    list.append(
-                        ('ATOM', count, '', 'H', '', 'ILE', '', 'A', count, '', '', i - 50, j - 50, k - 50, 1.0, 0,
-                         '', '', 'H', 0, count))
-                    count += 1
 
-    return list
+ppdb1 = lettura_file()
+ppdb1 = eliminazione_idrogeni(ppdb1)
 
+matrice = inizializzazione_matrice(ppdb1)
 
+griglia = inizializzazione_griglia(matrice)
 
-ppdb = letturaFile()
-ppdb = eliminazioneIdrogeni(ppdb)
+lista = creazione_lista_atomi(griglia)
 
-matrix = inizializzazioneMatrice()
+ppdb2 = fromdataframe_topdb(lista)
 
-grid = inizializzazioneGriglia()
-
-list = creazioneListaAtomi()
-
-pppdb = fromDataFrametoPdb(list)
-
-pppdb.to_pdb(path='./3eiy_stripped4.pdb',
-            records=['ATOM'],
-            gz=False,
-            append_newline=True)
+ppdb2.to_pdb(path='./3eiy_stripped4.pdb',
+             records=['ATOM'],
+             gz=False,
+             append_newline=True)
